dc05203f0539761e09147a732041d099b972b186
dagurtomas <dagurtomas@gmail.com>
2024-06-15 23:25:25 +0200

    add some examples

--- LeanCondensed/LightCondensed/Small.lean
+++ LeanCondensed/LightCondensed/Small.lean
@@ -1,5 +1,5 @@
 import Mathlib.CategoryTheory.Sites.Coherent.Equivalence
-import Mathlib.Condensed.Light.Basic
+import Mathlib.Condensed.Light.Module
 
 universe v u w
 
@@ -13,6 +13,12 @@ instance : LocallySmall.{max v w} (LightCondensed.{w} C) := by
   erw [locallySmall_congr ((equivSmallModel LightProfinite.{w}).sheafCongrPrecoherent C)]
   infer_instance
 
-example (A B : LightCondensed.{w} C) : Small.{max v w} (A ⟶ B) := LocallySmall.hom_small A B
+example (A B : LightCondensed.{w} C) : Small.{max v w} (A ⟶ B) := inferInstance
+
+example : LocallySmall.{u} LightCondSet.{u} := inferInstance
+
+example (R : Type) [Ring R] : LocallySmall.{0} (LightCondMod R) := inferInstance
+
+example : LocallySmall.{0} LightCondAb := inferInstance
 
 end LightCondensed
