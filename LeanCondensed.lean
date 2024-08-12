680aa176d33481e11ea83aac345f67a724456129
dagurtomas <dagurtomas@gmail.com>
2024-08-12 13:40:40 +0000

    mk_all

--- LeanCondensed.lean
+++ LeanCondensed.lean
@@ -3,6 +3,7 @@ import LeanCondensed.LightCondensed.Small
 import LeanCondensed.LightCondensed.Yoneda
 import LeanCondensed.Mathlib.CategoryTheory.Sites.DirectImage
 import LeanCondensed.Mathlib.Condensed.Adjunctions
+import LeanCondensed.Mathlib.Condensed.Light.Limits
 import LeanCondensed.Misc.NatFunctor
 import LeanCondensed.Misc.Promote
 import LeanCondensed.Misc.SheafHomForget
